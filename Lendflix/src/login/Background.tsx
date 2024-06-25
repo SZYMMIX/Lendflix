e5cc8bd0dcbb74c49a28e322ea74a331922061bd
SZYMMIX <szycim10@gmail.com>
2024-06-25 15:07:22 +0200

    Day2 patch3

--- Lendflix/src/login/Background.tsx
+++ Lendflix/src/login/Background.tsx
@@ -1,9 +1,16 @@
+import { Icon, Text } from '@fluentui/react';
 
 function Background(){
  return(
     <div className="img-background" id="login-background">
         <div className="login-card">
-        <h1>1232131</h1>
+        <Text className='start-text1' id='login-text'>Zaloguj się</Text>
+        <div className='login-inputs'>
+        <input type='email' name='email' minlength="5" maxlength="50" className='sign-up-input' id='login-input' placeholder='Adres e-mail'/>
+        <input type='password' name='password' id='login-input' className='sign-up-input' placeholder='Hasło'/>
+        <button className='login-button-L'>Zaloguj się</button>
+        <Text className='forgot-password-text'>Nie pamiętasz hasła?</Text>
+        </div>
         </div>
     </div>
  );
