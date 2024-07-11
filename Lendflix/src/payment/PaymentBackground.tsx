3d57d597e1a5ee658210cdd2021015d3847a2c48
SZYMMIX <szycim10@gmail.com>
2024-07-11 14:19:55 +0200

    Day14 patch2

--- Lendflix/src/payment/PaymentBackground.tsx
+++ Lendflix/src/payment/PaymentBackground.tsx
@@ -1,9 +1,19 @@
-import { Text } from '@fluentui/react';
+import { Icon, Text } from '@fluentui/react';
 
 function PaymentBackground(){
     return(
-    <div>
-        <Text>metoda płatności</Text>
+        <div className='payment-container'>
+    <div className='payment-back-button-container'>
+        <a href='account-info.html#/moje-konto'>
+        <button>
+        <Icon iconName="Back" className="back-to-account-icon"/>
+            <Text className='back-btn-text'>Konto </Text>
+        </button>
+        </a>
+    </div>
+    <div className='method-payment-container'>
+    <Text>metoda płatności </Text>
+    </div>
     </div>
     );
 }
--- Lendflix/src/payment/PaymentScript.tsx
+++ Lendflix/src/payment/PaymentScript.tsx
@@ -5,7 +5,7 @@ function PaymentScript(){
     return(
         <>
         <ChangeHeader/>
-        <div className="change-background">
+        <div className="payment-background">
             <PaymentBackground/>
         </div>
         <InfoFooter/>
