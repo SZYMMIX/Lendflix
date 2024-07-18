febbc37dfb8724be1b541323a306b5789e0f2cb9
SZYMMIX <szycim10@gmail.com>
2024-07-18 11:51:07 +0200

    Day19 patch1

--- Lendflix/src/menu/MovieTile.tsx
+++ Lendflix/src/menu/MovieTile.tsx
@@ -1,8 +1,19 @@
+import { useState } from "react";
+import { Icon, Text } from "@fluentui/react";
+
 function MovieTile({movieName, movieId}){
 
+const [isMovieHovered, setIsMovieHovered] = useState(false);
 return(
-    <div className="movie-image-container">
-    <img src={`./src/assets/${movieName}.jpg`} alt={`${movieName}`} className={`movie-tile-image ${movieId == "1" ? "movie-tile-first" : ""}`} />
+    <div className={`movie-image-container ${isMovieHovered ? "image-container-hovered" : ""}`}
+   >
+    <img src={`./src/assets/${movieName}.jpg`} alt={`${movieName}`} 
+         className={`movie-tile-image ${movieId == "1" ? "movie-tile-first" : ""}`} 
+         onMouseEnter={() => setIsMovieHovered(true)} onMouseLeave={() => setIsMovieHovered(false)}/>
+    <div className={`movie-tile-hovered-container `} id={`${isMovieHovered ? "tile-visible" : "tile-hidden"}`}
+    onMouseEnter={isMovieHovered ? () => setIsMovieHovered(true) : ""} onMouseLeave={isMovieHovered ? () => setIsMovieHovered(false) : ""}>
+    <Text>Tragedia</Text>
+    </div>
     </div>
 
 );
